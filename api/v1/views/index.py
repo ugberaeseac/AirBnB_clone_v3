53839900cf33e721a20bf8d0d1fb2d7e2451db5a
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-10-30 16:18:46 -0700

    debug index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,7 +17,7 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """
     returns JSON: "status": "OK"
@@ -25,17 +25,17 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     """
     retrieves the number of each objects by type
     """
     obj_count = {
-            "amenities": storage.count(Amenity),
-            "cities": storage.count(City),
-            "places": storage.count(Place),
-            "reviews": storage.count(Review),
-            "states": storage.count(State),
-            "users": storage.count(User)
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
             }
     return jsonify(obj_count)
