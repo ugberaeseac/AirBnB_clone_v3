dd91ecb01d75f333fd55e6ef8fc452840b2920f6
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-10-30 15:30:02 -0700

    revert changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 CORS(app, resources={r"/*": {"origins": "http://0.0.0.0"}})
