b7a520981e5fbf0bb96cf9dde94f63bc1119a18d
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-10-30 17:40:42 -0700

    debugging...

--- api/v1/app.py
+++ api/v1/app.py
@@ -36,4 +36,5 @@ def teardown(self):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
